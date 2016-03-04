a47db88afc0eca4ec538fee50418a59a36937020
Sang Phan <plsang@nii.ac.jp>
2016-03-03 23:56:35 -0900

    change linear layer --> conv layer

--- model_utils.lua
+++ model_utils.lua
@@ -14,23 +14,6 @@ function model_utils.load_vgg(opt)
     -- print('Removing the softmax layer')
     table.remove(vgg_model['modules'])  -- equivalent to pop
     
-    -- table.insert(vgg_model['modules'], nn.LogSoftMax():cuda())
-    
-    -- print(vgg_model['modules'])
-    
-    -- for k,v in pairs(vgg_model) do print(k,v) end
-    -- print('checking model by sampling temp input...')
-    -- print(#vgg_model:cuda():forward(torch.CudaTensor(1, 3, 224, 224)))
-
-    -- no longer need because image is now loaded using opencv, i.e., in BGR format
-    -- print('converting first layer conv filters from BGR to RGB...')
-    -- local input_layer = vgg_model:get(1)
-    -- local w = input_layer.weight:clone()
-    
-    -- swap weights to R and B channels
-    -- input_layer.weight[{ {}, 1, {}, {} }]:copy(w[{ {}, 3, {}, {} }])
-    -- input_layer.weight[{ {}, 3, {}, {} }]:copy(w[{ {}, 1, {}, {} }])
-    
     criterion = nn.MultilabelCrossEntropyCriterion():cuda()
     
     return vgg_model, criterion
@@ -166,5 +149,165 @@ function model_utils.define_vgg(opt)
     
     return model, criterion
 end
+
+
+-- Define a fine tuning network, that contains different groups.
+-- each group will have different learning rates
+function model_utils.finetune_vgg(opt)
+    local main_model = nn.Sequential()
+    -- group 1: frozen weight
+    local model = nn.Sequential()
+    
+    -- learing rate & weight decay multipliers for this group
+    model.w_lr_mult = 0
+    model.b_lr_mult = 0
+    model.w_wd_mult = 0
+    model.b_wd_mult = 0
+    
+    model:add(cudnn.SpatialConvolution(3, 64, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv1_1'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu1_1'
+    model:add(cudnn.SpatialConvolution(64, 64, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv1_2'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu1_2'
+    model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2, 0, 0):ceil())
+    model:get(#model).name = 'pool1'
+    model:add(cudnn.SpatialConvolution(64, 128, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv2_1'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu2_1'
+    model:add(cudnn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv2_2'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu2_2'
+    model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2, 0, 0):ceil())
+    model:get(#model).name = 'pool2'
+    
+    main_model:add(model)
+    model = nil
+    
+    -- group 2: normal weight
+    local model = nn.Sequential()
+    -- learing rate & weight decay multipliers for this group
+    model.w_lr_mult = 1
+    model.b_lr_mult = 2
+    model.w_wd_mult = 1
+    model.b_wd_mult = 0
+    
+    model:add(cudnn.SpatialConvolution(128, 256, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv3_1'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu3_1'
+    model:add(cudnn.SpatialConvolution(256, 256, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv3_2'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu3_2'
+    model:add(cudnn.SpatialConvolution(256, 256, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv3_3'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu3_3'
+    model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2, 0, 0):ceil())
+    model:get(#model).name = 'pool3'
+    model:add(cudnn.SpatialConvolution(256, 512, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv4_1'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu4_1'
+    model:add(cudnn.SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv4_2'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu4_2'
+    model:add(cudnn.SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv4_3'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu4_3'
+    model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2, 0, 0):ceil())
+    model:get(#model).name = 'pool4'
+    model:add(cudnn.SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv5_1'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu5_1'
+    model:add(cudnn.SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv5_2'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu5_2'
+    model:add(cudnn.SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1, 1))
+    model:get(#model).name = 'conv5_3'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu5_3'
+    model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2, 0, 0):ceil())
+    model:get(#model).name = 'pool5'
+    
+    model:add(cudnn.SpatialConvolution(512, 4096, 7, 7, 1, 1, 0, 0, 1))
+    model:get(#model).name = 'fc6'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu6'
+    model:add(nn.Dropout(0.500000))
+    model:get(#model).name = 'drop6'
+    
+    model:add(cudnn.SpatialConvolution(4096, 4096, 1, 1, 1, 1, 0, 0, 1))
+    model:get(#model).name = 'fc7'
+    model:add(cudnn.ReLU(true))
+    model:get(#model).name = 'relu7'
+    model:add(nn.Dropout(0.500000))
+    model:get(#model).name = 'drop7'
+    
+    main_model:add(model)
+    model = nil
+    
+    -- group 3: tuning weight
+    local model = nn.Sequential()
+    -- learing rate & weight decay multipliers for this group
+    model.w_lr_mult = 10
+    model.b_lr_mult = 20
+    model.w_wd_mult = 1
+    model.b_wd_mult = 0
+    
+    model:add(cudnn.SpatialConvolution(4096, 1000, 1, 1, 1, 1, 0, 0, 1))
+    model:get(#model).name = 'fc8'
+    
+    model:add(nn.View(-1):setNumInputDims(3)) -- 1x1x1000 --> 1000
+    model:get(#model).name = 'torch_view'
+    
+    model:add(nn.Sigmoid())
+    model:get(#model).name = 'sigmoid'
+    
+    main_model:add(model)
+    model = nil
+    
+    cudnn.convert(main_model, cudnn)
+    main_model:cuda()
+    
+    local parameters = main_model:getParameters() -- all params as one flat variables
+    
+    -- for k,v in pairs(model) do print (k,v) end
+    if opt.test_cp ~= '' then
+        print('loading network from a checkpoint ', opt.test_cp)
+        model_cp = torch.load(opt.test_cp)
+        local parameters_cp = model_cp.params
+        assert(parameters:nElement() == parameters_cp:nElement())
+        parameters:copy(parameters_cp)
+        model_cp = nil
+        parameters_cp = nil
+    else
+        print('loading network from a vgg pretrained model')
+        local model_vgg = model_utils.load_vgg(opt)
+        local parameters_vgg = model_vgg:getParameters() -- all params as one flat variables
+        print(parameters:nElement())
+        print(parameters_vgg:nElement())
+        assert(parameters:nElement() == parameters_vgg:nElement())
+        parameters:copy(parameters_vgg)
+        parameters_vgg = nil
+        model_vgg = nil
+    end
+    collectgarbage() 
+    
+    
+    criterion = nn.MultilabelCrossEntropyCriterion():cuda()
+    
+    return main_model, criterion
+end
+
 
 return model_utils
