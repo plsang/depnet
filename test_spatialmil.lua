8f7be24f66c143ec161ba47f5f338d792f8b85b9
Sang Phan <plsang@nii.ac.jp>
2016-03-28 23:03:57 -0900

    support testing mil indices

--- test_spatialmil.lua
+++ test_spatialmil.lua
@@ -10,8 +10,8 @@ require 'MILLayer'
 --torch.setdefaulttensortype('torch.FloatTensor')
 
 nb = 4 
-nt = 1000000
-mil_type = 'milnor'
+nt = 4096
+mil_type = 'milmax'
 
 x = torch.rand(nb,nt,12,12):uniform(0, 0.1)
 y = torch.rand(nb,nt)
@@ -28,6 +28,14 @@ print('Lua time: ', timer:time().real)
 timer = torch.Timer()
 m2 = nn.SpatialMIL(mil_type)
 m2:forward(x)
+for i=1,nb do
+    for j=1,nt do
+        local w = math.floor(m2.mil_indices[i][j]/12) + 1
+        local h = m2.mil_indices[i][j]%12 + 1
+        -- print(m2.mil_indices[i][j], w, h, x[i][j][w][h], m2.output[i][j])
+        assert(x[i][j][w][h] == m2.output[i][j])
+    end
+end
 m2:backward(x, y)
 print('C time: ', timer:time().real)
 
@@ -37,6 +45,14 @@ print('C time: ', timer:time().real)
 timer = torch.Timer()
 m3 = nn.SpatialMIL(mil_type):cuda()
 m3:forward(x:cuda())
+for i=1,nb do
+    for j=1,nt do
+        local w = math.floor(m3.mil_indices[i][j]/12) + 1
+        local h = m3.mil_indices[i][j]%12 + 1
+        -- print(m3.mil_indices[i][j], w, h, x[i][j][w][h], m3.output[i][j])
+        assert(x[i][j][w][h] - m3.output[i][j] < 1e-6)
+    end
+end
 m3:backward(x:cuda(), y:cuda())
 print('Cuda time: ', timer:time().real)
 
